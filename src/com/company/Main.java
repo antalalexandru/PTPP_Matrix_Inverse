c7d8ad3e5104948caeee7e4539762a5d8139e417
antal <crgroot97@gmail.com>
2019-04-15 11:14:33 +0300

    Savepoint

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,6 +2,7 @@ package com.company;
 
 import java.io.IOException;
 import java.math.BigDecimal;
+import java.util.Arrays;
 import java.util.Objects;
 
 public class Main {
@@ -17,29 +18,44 @@ public class Main {
 
     public static void main(String[] args) throws MatrixNotInvertibleException, BadArgumentException, IOException {
 
-        testAlgorithm();
+        //testAlgorithm();
 
-        String matrixFile = "./src/com/company/resources/matrix.txt";
-        String inverseMatrixFile = "./src/com/company/resources/inverse.txt";
+        //String matrixFile = "./src/com/company/resources/matrix.txt";
+        //String inverseMatrixFile = "./src/com/company/resources/inverse.txt";
 
-        int matrixSize = 3;
+        //int matrixSize = 3;
 
         //double[][] matrix = MatrixUtils.generateRandomMatrix(matrixSize, matrixSize);
         //MatrixUtils.writeMatrixToFile(matrixFile, matrix);
 
-        computeInverse(matrixFile, inverseMatrixFile, matrixSize);
+        //computeInverse(matrixFile, inverseMatrixFile, matrixSize);
+
+        Arrays.asList(5, 10, 25, 50, 100, 150, 200, 300, 500, 750, 1000, 2000, 2500, 3500, 5000).forEach(matrixSize -> {
+            double[][] matrix = MatrixUtils.generateRandomMatrix(matrixSize, matrixSize);
+
+            double result = TimeUtils.getTimeConsumption(() -> {
+                try {
+                    MatrixUtils.getMatrixInverseSecvential(matrix);
+                } catch (MatrixNotInvertibleException e) {
+                    e.printStackTrace();
+                }
+            }, 10);
+
+            System.out.printf("Matrix size: %d, duration: %f ms\n", matrixSize, result);
+
+        });
 
     }
 
     private static void testAlgorithm() throws MatrixNotInvertibleException, BadArgumentException {
-        for(int i = 0; i < 1000; i++) {
+        /*for(int i = 0; i < 1000; i++) {
             int matrixSize = 100;
             double[][] matrix = MatrixUtils.generateRandomMatrix(matrixSize, matrixSize);
             double[][] inverse = MatrixUtils.getMatrixInverseSecvential(matrix);
             if( ! MatrixUtils.matrixEquals(MatrixUtils.matrixMultiplication(matrix, inverse), MatrixUtils.getIdentityMatrix(matrixSize))) {
                 throw new RuntimeException("Wrong algorithm");
             }
-        }
+        }*/
     }
 
 }
--- src/com/company/TimeUtils.java
+++ src/com/company/TimeUtils.java
@@ -2,13 +2,12 @@ package com.company;
 
 public class TimeUtils {
     public static double getTimeConsumption(Runnable runnable, int numberOfTries) {
-        double totalTime = 0;
+        long totalTime = 0;
         for(int i = 0; i < numberOfTries; i++) {
             long start = System.currentTimeMillis();
             runnable.run();
-            long stop = System.currentTimeMillis();
-            totalTime += stop - start;
+            totalTime += System.currentTimeMillis() - start;
         }
-        return totalTime / numberOfTries;
+        return (1. * totalTime) / numberOfTries;
     }
 }
